fcc4f4f3370d2810fefcf2b300658b3d4eaf9d67
GAIN KIM <lina.kim727@likelion.org>
2017-08-25 15:08:41 +1000

    nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,9 @@ Rails.application.routes.draw do
 
   root 'projects#index'
 
-  get 'projects/index'
+  get 'index' => 'projects#index'
+
+  get 'calendar' => 'projects#calendar'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
